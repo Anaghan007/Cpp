86072f9cf0998c9cc4f0da4df56846eab032b8fb
whoisdarshan <femilkhuntk@gmail.com>
2024-05-22 08:15:29 +0530

    change

---
+++ AtoOnested.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+int main() 
+{ 
+    char a=65;
+	for (char i = 65; i <=69; i++) 
+	{ 
+	    for (char j = 65; j <=i; j++) 
+	    { 
+			cout <<a;
+            a++; 
+		} 
+		cout << endl; 
+	} 
+	return 0; 
+}
