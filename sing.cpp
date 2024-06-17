35bb332faca132a20ef7d673404fecb9826a1823
DevH1155 <devkumarhirpara07@gmail.com>
2024-06-17 09:53:25 +0530

    17-06-24

---
+++ sing.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+#include<stdlib.h>
+using namespace std;
+int main()
+{
+    // floor(2.6 , -2.9)
+    // ceil(2.3, -2.3)
+    // round(2.3, -5.3)
+    // pow(2,3)
+    // abs(-23, 23)
+    // rand()%50
+    cout<<"Ans :"<<floor(-2.9)<<endl;
+    cout<<"Ans :"<<floor(2.3)<<endl<<endl;
+    cout<<"Ans :"<<ceil(-2.9)<<endl;
+    cout<<"Ans :"<<ceil(2.3)<<endl<<endl;
+    cout<<"Ans :"<<round(-3.9)<<endl;
+    cout<<"Ans :"<<round(2.3)<<endl<<endl;
+    cout<<"Ans :"<<pow(3,3)<<endl<<endl;
+    cout<<"Ans :"<<abs(-23)<<endl;
+    cout<<"Ans :"<<abs(23)<<endl<<endl;
+    cout<<"Ans :"<<rand()<<endl;
+}
