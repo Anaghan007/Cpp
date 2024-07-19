783c81980dbb0ae9d6d8367cd4987aba54c48614
Yash <ustavpatel2004@gmail.com>
2024-07-19 09:38:24 +0530

    19-07-24

---
+++ Infix To Postfix.c
@@ -0,0 +1,23 @@
+// (A+(B-C)*(D-E)/F)->INFIX TO POSTFIX
+
+// Symbol      Stack       Exp
+// (           (
+// A           (           A
+// +           (+          A
+// (           (+(         A
+// B           (+(         AB
+// -           (+(-        AB
+// C           (+(-        ABC
+// )           (+          ABC-
+// *           (*          ABC-+
+// C           (*(         ABC-+
+// D           (*(         ABC-+D
+// -           (*(-        ABC-+D
+// E           (*(-        ABC-+DE
+// )           (*          ABC-+DE-
+// /           (/          ABC-+DE-*
+// F                       ABC-+DE-*F/
+
+// *Condition:-
+// 1.Symbole->stack->Big->As It Is
+// 1.Symbole->stack->Same/Small->Stack To Exp
